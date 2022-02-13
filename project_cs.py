b94b854b3964e3af850bff06d6d3846c4d634c07
rohannair2022 <99500406+rohannair2022@users.noreply.github.com>
2022-02-13 23:05:53 +0530

    Update project_cs.py

--- project_cs.py
+++ project_cs.py
@@ -1,69 +1,72 @@
 import mysql.connector as sql
 
-conn = sql.connect(host = "localhost", user="root", passwd="", database="")
-cursor = mycon.cursor()
+conn = sql.connect(host = "localhost", user="root", passwd="password", database="mysql")
+cursor = conn.cursor()
 
 def landing_page():
     print("Welcome to our Medical Website Project.")
     p_name = input("What is your name: ")
     print("Hi ", p_name, ", hope you are doing fine")
+    print("  ")
 
-
-def showlist()
+def showlist():
     print("Display all details of medicines available")
-    df = cursor.execute(" Select medicine from medi_project") 
+    print("  ")
+    cursor.execute(" Select medicine,code from medi_project")
+    df = cursor.fetchall()
     print(df)
-    
-def sortedmedicines()
+
+def sortedmedicines():
     print("These are the symptoms")
-    print("Symptom 1")
-    print("Symptom 2")
-    print("Symptom 3")
-    print("Symptom 4")
-    print("Symptom 5")
-    symptom = input("Type your symptom: ")
-    age = int(input("Type your age: "))
-    sex = input("Type your sex(m/f): ")
-    dl = cursor.execute("select * from medi_project where symptoms = {}, age > {}".format(symptoms, age))
+    print("headache")
+    print("fever")
+    print("aches and pain")
+    print("cold")
+    print("nausea")
+    symptoms = input("Type your symptom: ")
+    print("  ")
+    cursor.execute("select medicine,code from medi_project where symptoms = '{}' ".format(symptoms,))
+    dl= cursor.fetchall()
     print(dl)
 
 def continue_page():
-    continue_lpage = input(p_name," do you want to continue (press y for yes and n for no): ")
-    if continue_lpage == "y":
-        print(p_name," do you want to see the list of medines or do you want too find out the medicine required for you.")
-        choice = input("Type "1" if its the first and type "2" if it is the 2nd")
-            if choice == "1":
-                showlist()
-            elif choice == "2":
-                sortedmedicines()
-            else:
-                print("please type the correct value ", p_name)
-                continue_page()
+
+
+    print(" do you want to see the list of medines or do you want too find out the medicine required for you.")
+    print("  ")
+    choice = input("Type 1 if its the first and type 2 if it is the 2nd: ")
+    if choice == "1":
+        showlist()
+    elif choice == "2":
+        sortedmedicines()
     else:
-        print("Ok. Hope you have a nice day ", p_name)
+        print("please type the correct value ", p_name)
+        continue_page()
+
 
 def payment_page():
-    order = input("Enter your Medicine Code please : "))
-    quant = input("Enter the quantity: "))
+    print("  ")
+    order = input("Enter your Medicine Code please : ")
+    quant = int(input("Enter the quantity: "))
     if order == "m01":
-        total_price = 150*x
+        total_price = 150*quant
         print("You have bought ")
-        print("Your total is," total_price)
+        print("Your total is,",total_price)
     elif order == "m02":
-        total_price = 150*x
+        total_price = 150*quant
         print("You have bought ")
-        print("Your total is," total_price)
-    
+        print("Your total is,",total_price)
+
     elif order == "m03":
-        total_price = 150*x
+        total_price = 150*quant
         print("You have bought ")
-        print("Your total is," total_price)
-    
+        print("Your total is,",total_price)
+
     elif order == "m04":
-        total_price = 150*x
+        total_price = 150*quant
         print("You have bought ")
-        print("Your total is," total_price)
-        
+        print("Your total is,",total_price)
+
     else:
         print("Enter the correct value")
         payment_page()
@@ -71,7 +74,3 @@ def payment_page():
 landing_page()
 continue_page()
 payment_page()
-
-
-
-                    
